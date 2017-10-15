ed892121c058875e2cff0f4aa455cbc4dde4d4fb
ilgwonPark <dlfrnjs1004@naver.com>
2017-10-15 14:02:05 +0900

    fix #9 main - design draft
    draft

--- website_v2/src/components/main.vue
+++ website_v2/src/components/main.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div class="">
     <!-- header -->
-    <section class="bus_stop_header">
-      <div class="row bus_container_content">
+    <section class="row bus_stop_header">
+      <div class="bus_container_content">
         <div class="col-sm-6" id="bus_stop_name">한이음버스환승센터</div>
         <div class="col-sm-2" id="bus_stop_id">12-345</div>
         <div class="col-sm-2" id="bus_stop_system">
@@ -15,109 +15,109 @@
     <!--main body-->
     <section>
       <div class="row bus_container">
-        <div class="col-sm-6">
-          <div class="col-sm-6">
-            마을01
+        <div class="col-sm-6 bus_container_border_general bus_container_border_town">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
+            마을 01
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             14분
           </div>
         </div>
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             105
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             41분
           </div>
         </div>
       </div>
       <div class="row bus_container">
-        <div class="col-sm-6">
-          <div class="col-sm-6">
-            마을02
+        <div class="col-sm-6 bus_container_border_general bus_container_border_town">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
+            마을 02
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             27분
           </div>
         </div>
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             106
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             15분
           </div>
         </div>
       </div>
       <div class="row bus_container">
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             101
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             38분
           </div>
         </div>
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_green">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             1001
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             19분
           </div>
         </div>
       </div>
       <div class="row bus_container">
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             102
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             7분
           </div>
         </div>
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_green">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             1002
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             24분
           </div>
         </div>
       </div>
       <div class="row bus_container">
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             103
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             54분
           </div>
         </div>
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_green">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             1003
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             31분
           </div>
         </div>
       </div>
       <div class="row bus_container">
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             104
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             12분
           </div>
         </div>
-        <div class="col-sm-6">
-          <div class="col-sm-6">
+        <div class="col-sm-6 bus_container_border_general bus_container_border_red">
+          <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
             9001
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-5 bus_container_background_general">
             42분
           </div>
         </div>
