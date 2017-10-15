e0ae9ccea16ef0f35d222d1a8a753e5d55b63b20
ilgwonPark <dlfrnjs1004@naver.com>
2017-10-15 16:41:56 +0900

    fix #11 main -popup_reserve UI
    main -popup_reserve UI

--- website_v2/src/components/main.vue
+++ website_v2/src/components/main.vue
@@ -17,7 +17,7 @@
       <div class="row bus_container">
         <div class="col-sm-6 bus_container_border_general bus_container_border_town">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >마을 01</button>
+            <a class= "button" href="#popup1">마을 01</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             14분
@@ -25,7 +25,7 @@
         </div>
         <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >105</button>
+            <a class= "button" href="#popup1">105</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             41분
@@ -35,7 +35,7 @@
       <div class="row bus_container">
         <div class="col-sm-6 bus_container_border_general bus_container_border_town">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >마을 02</button>
+            <a class= "button" href="#popup1">마을 02</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             27분
@@ -43,7 +43,7 @@
         </div>
         <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >106</button>
+            <a class= "button" href="#popup1">106</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             15분
@@ -53,7 +53,7 @@
       <div class="row bus_container">
         <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >101</button>
+            <a class= "button" href="#popup1">101</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             38분
@@ -61,7 +61,7 @@
         </div>
         <div class="col-sm-6 bus_container_border_general bus_container_border_green">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >1001</button>
+            <a class= "button" href="#popup1">1001</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             19분
@@ -71,7 +71,7 @@
       <div class="row bus_container">
         <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >102</button>
+            <a class= "button" href="#popup1">102</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             7분
@@ -79,7 +79,7 @@
         </div>
         <div class="col-sm-6 bus_container_border_general bus_container_border_green">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >1002</button>
+            <a class= "button" href="#popup1">1002</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             24분
@@ -89,7 +89,7 @@
       <div class="row bus_container">
         <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >103</button>
+            <a class= "button" href="#popup1">103</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             54분
@@ -97,7 +97,7 @@
         </div>
         <div class="col-sm-6 bus_container_border_general bus_container_border_green">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >1003</button>
+            <a class= "button" href="#popup1">1003</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             31분
@@ -107,7 +107,7 @@
       <div class="row bus_container">
         <div class="col-sm-6 bus_container_border_general bus_container_border_blue">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >104</button>
+            <a class= "button" href="#popup1">104</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             12분
@@ -115,15 +115,53 @@
         </div>
         <div class="col-sm-6 bus_container_border_general bus_container_border_red">
           <div class="col-sm-7 bus_container_background_general bus_container_background_bus">
-            <button type="button" name="button" >9001</button>
+            <a class= "button" href="#popup1">9001</a>
           </div>
           <div class="col-sm-5 bus_container_background_general">
             42분
           </div>
         </div>
       </div>
-
     </section>
+
+
+    <div id="popup1" class="overlay">
+	     <div class="popup">
+		       <!-- <h2>Here i am</h2> -->
+		       <!-- <a class="close" href="#">&times;</a> -->
+		       <!-- <div class="content">
+			       Thank to pop me out of that button, but now i'm done so you can close this window.
+		       </div> -->
+           <div class="col-sm-6 popup_container_border_top">
+             <div class="row popup_container_top_top">
+               <div class="col-sm-8">
+                 <h2>마을 01</h2>
+               </div>
+               <div class="col-sm-4">
+                 <h5>
+                   마을버스<br/>
+                   청량리행
+                 </h5>
+               </div>
+             </div>
+             <div class="row popup_container_top_bottom">
+               <div class="col-sm-12">
+                 <h5>
+                    저상버스 승차를 예약하시겠습니까?
+                 </h5>
+               </div>
+             </div>
+           </div>
+           <div class="col-sm-6 popup_container_bottom">
+             <div class="col-sm-6 popup_container_bottom_left">
+               <a class="confirm" href="#" style="padding-left:10vh; color:black;">예</a>
+             </div>
+             <div class="col-sm-6 popup_container_bottom_right">
+               <a class="cancel" href="#" style="padding-left:6vh; color:black;">아니오</a>
+             </div>
+           </div>
+	    </div>
+    </div>
   </div>
 </template>
 
